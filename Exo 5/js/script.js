cd9b6b67698c40ef8fd7e7841db789116072f03b
Lowari <66225181+Lowari@users.noreply.github.com>
2020-11-02 12:31:23 +0100

    Rajout console log test

--- Exo 5/js/script.js
+++ Exo 5/js/script.js
@@ -3,3 +3,4 @@ let secondNumber = prompt('Donnez moi un autre chiffre');
 let result = Number(firstNumber) + Number(secondNumber);
 alert(`Le resultat de ${firstNumber} et de ${secondNumber} est de ${result}`);
 console.log(`Le resultat de ${firstNumber} et de ${secondNumber} est de ${result}`);
+console.log("test");
